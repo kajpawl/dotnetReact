8c535a4c778be91ff218e83cc72572f0297d15d2
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-04 20:53:53 +0100

    Fix client app project structure; add basic styling

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { List, Container } from "semantic-ui-react";
+import NavBar from "../../features/nav/NavBar";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
