e33ccbfff996ca24abbce1dff0adc65dd9ebe279
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-04 23:22:21 +0100

    Client: select and display individual activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,16 +7,22 @@ import ActivityForm from "../form/ActivityForm";
 
 interface IProps {
   activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList activities={activities} />
+        <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <ActivityDetails />
+        {selectedActivity && <ActivityDetails activity={selectedActivity} />}
         <ActivityForm />
       </Grid.Column>
     </Grid>
