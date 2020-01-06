293fd56b11820a99c790281835b6fac9c8c6a415
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-06 19:01:17 +0100

    Client: add createActivity and selectActivity in MobX

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,18 +5,47 @@ import agent from "../api/agent";
 
 class ActivityStore {
   @observable activities: IActivity[] = [];
+  @observable selectedActivity: IActivity | undefined;
   @observable loadingInitial = false;
+  @observable editMode = false;
+  @observable submitting = false;
 
-  @action loadActivities = () => {
+  @action loadActivities = async () => {
     this.loadingInitial = true;
-    agent.Activities.list()
-      .then(activities => {
-        activities.forEach(activity => {
-          activity.date = activity.date.split(".")[0];
-          this.activities.push(activity);
-        });
-      })
-      .finally(() => (this.loadingInitial = false));
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach(activity => {
+        activity.date = activity.date.split(".")[0];
+        this.activities.push(activity);
+      });
+      this.loadingInitial = false;
+    } catch (error) {
+      this.loadingInitial = false;
+      console.log(error);
+    }
+  };
+
+  @action createActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.create(activity);
+      this.activities.push(activity);
+      this.editMode = false;
+      this.submitting = false;
+    } catch (error) {
+      this.submitting = false;
+      console.log(error);
+    }
+  };
+
+  @action openCreateForm = () => {
+    this.editMode = true;
+    this.selectedActivity = undefined;
+  };
+
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find(a => a.id === id);
+    this.editMode = false;
   };
 }
 
