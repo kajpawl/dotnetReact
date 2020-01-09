5a28aef51dbc0698ad06840e1360642a49c907a4
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-09 22:59:42 +0100

    Client: save JWT token to local storage and pass it to app

--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -1,5 +1,6 @@
 import ActivityStore from "./activityStore";
 import UserStore from "./userStore";
+import CommonStore from "./commonStore";
 import { createContext } from "react";
 import { configure } from "mobx";
 
@@ -8,10 +9,12 @@ configure({ enforceActions: "always" });
 export class RootStore {
   activityStore: ActivityStore;
   userStore: UserStore;
+  commonStore: CommonStore;
 
   constructor() {
     this.activityStore = new ActivityStore(this);
     this.userStore = new UserStore(this);
+    this.commonStore = new CommonStore(this);
   }
 }
 
