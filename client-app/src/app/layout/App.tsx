0d35918c41700caae801bb2064c383fac1c78bd8
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-09 22:33:52 +0100

    Client: create login form

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,6 +9,7 @@ import HomePage from "../../features/home/HomePage";
 import NotFound from "./NotFound";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import { ToastContainer } from "react-toastify";
+import LoginForm from "../../features/user/LoginForm";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
@@ -29,6 +30,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
+                <Route path="/login" component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,7 +1,8 @@
-import { observable, action, computed } from "mobx";
+import { observable, action, computed, runInAction } from "mobx";
 import { IUser, IUserFormValues } from "../models/user";
 import agent from "../api/agent";
 import { RootStore } from "./rootStore";
+import { history } from "../..";
 
 export default class UserStore {
   rootStore: RootStore;
@@ -18,9 +19,13 @@ export default class UserStore {
   @action login = async (values: IUserFormValues) => {
     try {
       const user = await agent.User.login(values);
-      this.user = user;
+      runInAction(() => {
+        this.user = user;
+      });
+      console.log(user);
+      history.push("/activities");
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   };
 }
