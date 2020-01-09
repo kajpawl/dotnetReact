2e1985b20ffc1f2a9fb7eb5a7a5592e490dd47ac
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2020-01-09 15:50:12 +0100

    Server: retrieve username from the token

--- API/Startup.cs
+++ API/Startup.cs
@@ -75,6 +75,7 @@ namespace API
                 });
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
