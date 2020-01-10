4a6431e6ba62d9139352295ec1ddbb51525eaa3a
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2020-01-10 15:16:23 +0100

    Add registration form and manage errors

--- Application/Validators/ValidatorExtensions.cs
+++ Application/Validators/ValidatorExtensions.cs
@@ -9,9 +9,9 @@ namespace Application.Validators
             var options = ruleBuilder
                 .NotEmpty()
                 .MinimumLength(6).WithMessage("Password must be at lest 6 characters")
-                .Matches("A-Z").WithMessage("Password must contain 1 uppercase letter")
-                .Matches("a-z").WithMessage("Password must contain 1 lowercase character")
-                .Matches("0-9").WithMessage("Password must contain a number")
+                .Matches("[A-Z]").WithMessage("Password must contain 1 uppercase letter")
+                .Matches("[a-z]").WithMessage("Password must contain 1 lowercase character")
+                .Matches("[0-9]").WithMessage("Password must contain a number")
                 .Matches("[^a-zA-Z0-9]").WithMessage("Password must non alphanumeric");
 
             return options;
