c9ede524ec03fb51252c02cf46d89e56adcd0db7
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-10 20:22:08 +0100

    Server: add UserActivity class, update create activity handler and db

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -37,9 +39,12 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
-                this._context = context;
+                _userAccessor = userAccessor;
+                _context = context;
             }
 
             public async Task<Unit> Handle(Command request,
@@ -57,6 +62,20 @@ namespace Application.Activities
                 };
 
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.getCurrentUsername());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendee);
+
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
