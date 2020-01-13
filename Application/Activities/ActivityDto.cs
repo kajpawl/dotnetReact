ee1eb19a0e953d0481141bdb2c32ff3f14f005ff
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2020-01-13 17:07:33 +0100

    Server: Add entity, DTO, mapping and handler for Comment

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Application.Comments;
 
 namespace Application.Activities
 {
@@ -13,8 +14,9 @@ namespace Application.Activities
         public DateTime Date { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        
+
         [JsonPropertyName("attendees")]
         public ICollection<AttendeeDto> UserActivities { get; set; }
+        public ICollection<CommentDto> Comments { get; set; }
     }
 }
