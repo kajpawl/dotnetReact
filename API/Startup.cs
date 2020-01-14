769be3f38ca629e9ba4bec3e967e84a17fe33ae1
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-14 22:37:02 +0100

    Client: Fix bugs: login on token expiry, activity form

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using API.Middleware;
@@ -48,7 +49,12 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000").AllowCredentials();
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithExposedHeaders("WWW-Authenticate")
+                        .WithOrigins("http://localhost:3000")
+                        .AllowCredentials();
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
@@ -89,7 +95,9 @@ namespace API
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = key,
                         ValidateAudience = false,
-                        ValidateIssuer = false
+                        ValidateIssuer = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
                     };
                     opt.Events = new JwtBearerEvents
                     {
--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -22,9 +22,19 @@ axios.interceptors.response.use(undefined, error => {
   if (error.message === "Network Error" && !error.response) {
     toast.error("Network error - make sure API is running!");
   }
-  const { status, data, config } = error.response;
+  const { status, data, config, headers } = error.response;
   if (status === 404) {
     history.push("/notfound");
+  }
+  if (
+    status === 401 &&
+    headers["www-authenticate"].includes(
+      `Bearer error="invalid_token", error_description="The token`
+    )
+  ) {
+    window.localStorage.removeItem("jwt");
+    history.push("/");
+    toast.info("Your session has expired, please login again")
   }
   if (status === 400 && config.method === "get" && data.errors.hasOwnProperty("id")) {
     history.push("/notfound");
